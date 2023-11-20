c88a268b3c235db6268f748b68e48f6d09a5e244
Womby <realwombynator@gmail.com>
2023-11-20 18:30:34 +0100

    Updated Logger

--- src/utilities/logger.cpp
+++ src/utilities/logger.cpp
@@ -1,49 +1,85 @@
-#include <SFML/System/Clock.hpp>
-#include <SFML/System/Time.hpp>
 #include "logger.h"
 
+
 std::ofstream fileStream;
-sf::Clock appClock;
+std::ofstream outputFile("output.txt");
+std::streambuf* originalOut;
+auto start = std::chrono::high_resolution_clock::now();
+
+
+std::string getCurrentDateTimeString(const std::string& dateSeparator, const std::string& dateTimeSeparator, const std::string& timeSeparator, const std::string& msSeparator, bool includeMilliseconds) {
+    // Get the current system time
+    auto now = std::chrono::system_clock::now();
+
+    // Convert the system time to a timepoint
+    std::time_t currentTime = std::chrono::system_clock::to_time_t(now);
+
+    // Initialize a tm struct to store the time components
+    std::tm timeInfo;
+
+    // Use localtime_s to fill the tm struct (thread-safe version)
+    if (localtime_s(&timeInfo, &currentTime) != 0) {
+        // Handle error if localtime_s fails
+        return "Error: Unable to get local time.";
+    }
+
+    // Format the components into a string
+    std::ostringstream oss;
+    oss << std::setfill('0');
+    oss << std::setw(4) << timeInfo.tm_year + 1900 << dateSeparator;
+    oss << std::setw(2) << timeInfo.tm_mon + 1 << dateSeparator;
+    oss << std::setw(2) << timeInfo.tm_mday << dateTimeSeparator;
+    oss << std::setw(2) << timeInfo.tm_hour << timeSeparator;
+    oss << std::setw(2) << timeInfo.tm_min << timeSeparator;
+    oss << std::setw(2) << timeInfo.tm_sec;
+
+    if (includeMilliseconds) {
+        oss << msSeparator;
+        oss << std::setw(3) << std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() % 1000;
+    }
+
+    return oss.str();
+}
+
 
 void Log(MessageType type, const std::string& source, const std::string& message) {
 
     // Get TimeStamp
-    auto now = std::chrono::system_clock::now();
-    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-    auto now_ms_value = now_ms.time_since_epoch().count();
-    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-    std::tm* now_tm = std::localtime(&now_time_t);
-    int milliseconds = static_cast<int>(now_ms_value % 1000);
-    std::ostringstream oss;
-    oss << std::put_time(now_tm, "%Y-%m-%d %H:%M:%S");
-    oss << '.' << std::setw(3) << std::setfill('0') << milliseconds;
-    std::string timestamp = oss.str();
+    std::string timestamp = getCurrentDateTimeString("-", " ", ":", ".", true);
 
     // Get Message Type
     std::string messageType;
+    std::string messageColor;
     switch (type) {
-        case MessageType::NONE:
-            messageType = "";
-            break;
-        case MessageType::TRACE:
-            messageType = "[TRACE] ";
-            break;
-        case MessageType::DEBUG:
-            messageType = "[DEBUG] ";
-            break;
-        case MessageType::INFO:
-            messageType = "[INFO] ";
-            break;
-        case MessageType::WARN:
-            messageType = "[WARN] ";
-            break;
-        case MessageType::ERROR:
-            messageType = "[ERROR] ";
-            break;
-        case MessageType::FATAL:
-            messageType = "[FATAL] ";
-            break;}
-    
+    case MessageType::None:
+        messageType = "";
+        break;
+    case MessageType::Trace:
+        messageType = "[TRACE] ";
+        messageColor = "\033[38;5;175m"; //Purple
+        break;
+    case MessageType::Debug:
+        messageType = "[DEBUG] ";
+        messageColor = "\033[38;5;111m"; //Blue
+        break;
+    case MessageType::Info:
+        messageType = "[INFO] ";
+        messageColor = "\033[38;5;112m"; //Green
+        break;
+    case MessageType::Warning:
+        messageType = "[WARN] ";
+        messageColor = "\033[38;2;255;170;00m"; //Orange
+        break;
+    case MessageType::Error:
+        messageType = "[ERROR] ";
+        messageColor = "\033[38;2;255;85;85m"; //Red
+        break;
+    case MessageType::Fatal:
+        messageType = "[FATAL] ";
+        messageColor = "\033[48;2;170;0;0m"; //Dark Red Background
+        break;
+    }
+
     // Get Source
     std::string formattedSource = source;
     if (!formattedSource.empty()) {
@@ -51,38 +87,33 @@ void Log(MessageType type, const std::string& source, const std::string& message
     }
 
     // Log Message
+    std::cout.rdbuf(originalOut);
     fileStream << "[" << timestamp << "] " << formattedSource << messageType << message << std::endl;
-    std::cout << "[" << timestamp << "] " << formattedSource << messageType << message << std::endl;
+    std::cout << "\033[38;5;112m" << "[" << timestamp << "] " << "\033[38;5;111m" << formattedSource << "\033[0m" << messageType  << messageColor << message << "\033[0m" << std::endl;
+    originalOut = std::cout.rdbuf(outputFile.rdbuf());
 }
 
 void InitLogger() {
-    // Get Filename
-    auto now = std::chrono::system_clock::now();
-    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
-    std::tm* now_tm = std::localtime(&now_time_t);
-    std::ostringstream oss;
-    oss << std::put_time(now_tm, "%Y%m%d_%H%M%S");
-    std::string filename = oss.str();
+    // Redirect stdout to a file
+    originalOut = std::cout.rdbuf(outputFile.rdbuf());
 
-    fileStream.open(("logs/" + filename + ".log"), std::ios::app);
-
-    // Get TimeStamp
-    auto now_ms = std::chrono::time_point_cast<std::chrono::milliseconds>(now);
-    auto now_ms_value = now_ms.time_since_epoch().count();
-    int milliseconds = static_cast<int>(now_ms_value % 1000);
-    std::ostringstream ossms;
-    ossms << std::put_time(now_tm, "%Y-%m-%d %H:%M:%S");
-    ossms << '.' << std::setw(3) << std::setfill('0') << milliseconds;
-    std::string timestamp = "Started new log on " + ossms.str();
+    // Create log file: YYYY-MM-DD_HH-MM-SS.log
+    fileStream.open(("logs/" + getCurrentDateTimeString("-", "_", "-", " ", false) + ".log"), std::ios::app);
 
     // Start Log
-    Log(MessageType::NONE, "", timestamp);
-    Log(MessageType::NONE, "", "====================================================================================================");
+    Log(MessageType::None, "", ("Started new log on " + getCurrentDateTimeString("-", " ", ":", ".", true)));
+    Log(MessageType::None, "", "==============================================================================================");
 }
 
+// Log Message: [YYYY-MM-DD HH:MM:SS.MS] Stopping! Time elapsed: XXXX ms.
+// Closes File
 void CleanUpLogger() {
-    sf::Time elapsedTime = appClock.getElapsedTime();
-    Log(MessageType::NONE, "", ("Stopping! Time elapsed: " + std::to_string((int)(elapsedTime.asSeconds() * 1000)) + " ms."));
+    int duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - start).count();
+    int hours = duration / 1000 / 60 / 60;
+    int minutes = (duration - hours * 1000 * 60 * 60) / 1000 / 60;
+    int seconds = (duration - hours * 1000 * 60 * 60 - minutes * 1000 * 60) / 1000;
+    int rest = duration % 1000;
+    Log(MessageType::None, "", ("Stopping! Time elapsed: " + std::to_string(duration) + " ms. (" + std::to_string(hours) + "h " + std::to_string(minutes) + "m " + std::to_string(seconds) + "s " + std::to_string(rest) + "ms)"));
     if (fileStream.is_open())
         fileStream.close();
 }
--- src/utilities/logger.h
+++ src/utilities/logger.h
@@ -4,24 +4,47 @@
 #include <iostream>
 #include <fstream>
 #include <ctime>
-#include <string>
 #include <chrono>
 #include <iomanip>
 #include <sstream>
 
 enum class MessageType {
-    NONE,
-    TRACE,
-    DEBUG,
-    INFO,
-    WARN,
-    ERROR,
-    FATAL
+    None,
+    Trace,
+    Debug,
+    Info,
+    Warning,
+    Error,
+    Fatal
 };
+
+/**
+ * @brief Logs a message with the specified type, source, and message content.
+ *
+ * This function is used to log messages of different types, such as informational,
+ * warning, or error messages, to provide visibility into the program's behavior.
+ *
+ * @param type    The type of the message (e.g., MessageType::Info, MessageType::Warning, MessageType::Error).
+ * @param source  The source or context where the message originated (e.g., function name, module name).
+ * @param message The actual message content to be logged.
+ *
+ * Usage Example:
+ *
+ *     // Logging an informational message
+ *     Log(MessageType::Info, "MainFunction", "Program started successfully.");
+ *
+ *     // Logging a warning message
+ *     Log(MessageType::Warning, "DataProcessor", "Input data format is not recognized.");
+ *
+ *     // Logging an error message
+ *     Log(MessageType::Error, "FileIO", "Failed to open the input file.");
+ *
+ * Ensure that the necessary setup for logging (e.g., configuring log levels, output destinations)
+ * is done before using this function to ensure proper logging behavior.
+ */
+void Log(MessageType type, const std::string& source, const std::string& message);
 
 void InitLogger();
 void CleanUpLogger();
-
-void Log(MessageType type, const std::string& source, const std::string& message);
 
 #endif
